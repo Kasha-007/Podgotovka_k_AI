467318e9b19adf081b04740ab54afad01421da94
Яша <yakov_003@mail.ru>
2021-07-15 21:53:08 +0400

    First window

--- Application_for_Windows/First_page.py
+++ Application_for_Windows/First_page.py
@@ -1,3 +1,17 @@
 # Hi, my name is Yasha and this is my first English project
-import PyQt5
+# import PyQt5
 # Make changes in browser
+# Check changes in Pycharm
+# Ok, changes are visible
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(250, 250)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+
+    sys.exit(app.exec())
