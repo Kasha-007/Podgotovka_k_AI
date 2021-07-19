c3edfa5091b4de7594cbb24d79ef117a1f900b09
Яша <yakov_003@mail.ru>
2021-07-19 21:40:54 +0400

    Add Icon

--- Application_for_Windows/First_page.py
+++ Application_for_Windows/First_page.py
@@ -5,13 +5,23 @@
 # Ok, changes are visible
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('web.png'))
+
+        self.show()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = QWidget()
-    w.resize(250, 250)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-
-    sys.exit(app.exec())
+    ex = Example()
+    sys.exit(app.exec_())
